4cf634823195bdcd5babf9de5c3b0f08608570cf
César <davicesar602@gmail.com>
2025-03-12 17:05:11 -0300

    feito

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-wad;
+function alterarStatus(id) {
+  let jogoClicado = document.getElementById(`game-${id}`);
+  let imagem = jogoClicado.querySelector(".dashboard__item__img");
+  let botao = jogoClicado.querySelector(".dashboard__item__button");
+
+  if (botao.innerText === "Alugar") {
+    botao.innerText = "Devolver";
+    botao.classList.add("dashboard__item__button--return");
+    imagem.classList.add("dashboard__item__img--rented");
+  } else {
+    botao.innerText = "Alugar";
+    botao.classList.remove("dashboard__item__button--return");
+    imagem.classList.remove("dashboard__item__img--rented");
+  }
+}
