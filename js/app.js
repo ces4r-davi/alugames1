8ded42b1dc2189c7e5f77090ab5f5baa7caa6082
César <davicesar602@gmail.com>
2025-03-12 19:53:20 -0300

    aa

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
+let jogosAlugados = 0;
 function alterarStatus(id) {
   let jogoClicado = document.getElementById(`game-${id}`);
   let imagem = jogoClicado.querySelector(".dashboard__item__img");
   let botao = jogoClicado.querySelector(".dashboard__item__button");
 
   if (botao.innerText === "Alugar") {
+    jogosAlugados++;
+    console.log(jogosAlugados);
     botao.innerText = "Devolver";
     botao.classList.add("dashboard__item__button--return");
     imagem.classList.add("dashboard__item__img--rented");
-  } else {
-    botao.innerText = "Alugar";
-    botao.classList.remove("dashboard__item__button--return");
-    imagem.classList.remove("dashboard__item__img--rented");
+  } else if (botao.innerText === "Devolver") {
+    botao.innerText = "Tem certeza da devolução?";
+    botao.dataset.confirmacao = "true"; // Marca que está aguardando confirmação
+  } else if (botao.innerText === "Tem certeza da devolução?") {
+    if (botao.dataset.confirmacao === "true") {
+      botao.innerText = "Alugar";
+      botao.classList.remove("dashboard__item__button--return");
+      imagem.classList.remove("dashboard__item__img--rented");
+      delete botao.dataset.confirmacao; // Reseta a confirmação
+    }
   }
 }
